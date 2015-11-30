b90549e74691135be0fc9619f7a47a2ef075c754
Nathan Kozyra <nkozyra@gmail.com>
2015-11-29 23:54:15 -0500

    Adds imports & arguments

--- smartcrop.py
+++ smartcrop.py
@@ -1,5 +1,25 @@
 import numpy as np
-import cv2
+import cv2, getopt, sys
+from matplotlib import pyplot as plt
 
 def autocrop(image):
   pass
+
+
+def main(argv):
+  face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
+  eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+  inputFile = ''
+  outputFile = ''
+  width = 300
+  height = 300
+  opts, args = getopt.getopt(argv,"hi:o:w:h:",["ifile=","ofile="])
+  for opt, arg in opts:
+    if opt == "-i":
+      inputFile = arg
+    if opt == "-o":
+      outputFile = arg
+    if opt == "-h":
+      height = arg
+    if opt == "-w":
+      width = arg
