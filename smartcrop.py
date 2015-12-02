5532e66a2e6439e7658be3eb570a32317ae14cd0
Nathan Kozyra <nkozyra@gmail.com>
2015-12-02 00:46:41 -0500

    Multipass

--- smartcrop.py
+++ smartcrop.py
@@ -7,18 +7,74 @@ from matplotlib import pyplot as plt
 
 def detectEdges(image):
   edges = cv2.Canny(image,300,300)
-  plt.subplot(121),plt.imshow(image,cmap = 'gray')
-  plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-  plt.subplot(122),plt.imshow(edges,cmap = 'gray')
-  plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+  # plt.subplot(121),plt.imshow(image,cmap = 'gray')
+  # plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+  # plt.subplot(122),plt.imshow(edges,cmap = 'gray')
+  # plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
   # edgeMap = plt * 255
   edges = cv2.GaussianBlur(edges,(5,5),0)
   edges = cv2.GaussianBlur(edges,(5,5),0)
+  image = edges
   cv2.imwrite("fah.jpg",edges)
 
-def findUnique(image):
-  # blocks = 
-  print image[0]
+def isolateUnique(image):
+  blocksize = 32
+  varianceThreshold = 95
+  cellPx = image.shape[1] / blocksize
+  rows = image.shape[0] / cellPx
+  cols = blocksize
+  blockPx = cellPx * cellPx
+  cellValues = [0] * (rows)
+  imgR = 0
+  imgG = 0
+  imgB = 0
+  imgCells = (blockPx) * (rows * cols)
+  for i in range(rows):
+    cellValues[i] = [0] * blocksize
+    for j in range(cols):
+      rbeg = cellPx * i
+      rend = rbeg + cellPx
+
+      cbeg = cellPx * j
+      cend = cbeg + cellPx
+      r = 0
+      g = 0
+      b = 0
+      for ii in range(rbeg,rend):
+        for jj in range(cbeg,cend):
+          r = r + image[ii][jj][0]
+          g = g + image[ii][jj][1]
+          b = b + image[ii][jj][2]
+
+      imgR = imgR + r
+      imgG = imgG + g
+      imgB = imgB + b
+      rv = (r/blockPx)
+      gv = (g/blockPx)
+      bv = (b/blockPx)
+      cellValues[i][j] = [rv,gv,bv]
+      cv2.rectangle(image, (cbeg,rbeg), (cend,rend), (rv,gv,bv), -1)
+  avgR = (imgR/imgCells)
+  avgG = (imgG/imgCells)
+  avgB = (imgB/imgCells)
+  # cv2.rectangle(image, (100,100), (200,200), (avgR,avgG,avgB), -1)
+  for i in range(len(cellValues)):
+    for j in range(len(cellValues[i])):
+      rdiff = abs(cellValues[i][j][0] - avgR)
+      gdiff = abs(cellValues[i][j][1] - avgG)
+      bdiff = abs(cellValues[i][j][2] - avgB)
+      rbeg = cellPx * i
+      rend = rbeg + cellPx
+
+      cbeg = cellPx * j
+      cend = cbeg + cellPx
+      pxDiff = True if (rdiff > varianceThreshold or gdiff > varianceThreshold or bdiff > varianceThreshold) else False
+      isBlack = True if (cellValues[i][j][0] < 30 and cellValues[i][j][1] < 30 and cellValues[i][j][2] < 30) else False
+      if pxDiff and ( isBlack == False):
+        cv2.rectangle(image, (cbeg,rbeg), (cend,rend), (255,255,255), -1)
+      else:
+        cv2.rectangle(image, (cbeg,rbeg), (cend,rend), (0,0,0), -1)
+  cv2.imwrite("blocks.png",image)
 
 def main(argv):
   face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
@@ -47,7 +103,7 @@ def main(argv):
       flags = cv2.cv.CV_HAAR_SCALE_IMAGE
   )
   detectEdges(img)
-  findUnique(img)
+  isolateUnique(img)
   maxFaceX = 0
   maxFaceCenter = 0
   maxFaceRight = 0
@@ -57,7 +113,7 @@ def main(argv):
   imHeight,imWidth = img.shape[:2]
   cropBias = int(imWidth)
   for (x,y,w,h) in faces:
-    cv2.rectangle(img,(x,y),(x+w,y+h),(25,0,255),2)
+    cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,255),-1)
     cropBias = cropBias - x
     if (x+w) > maxFaceX:
       maxFaceX = x + w
